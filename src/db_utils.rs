e2308d089866c7c948b0f962213ab483a4e098ad
lukasbeckercode <lukas.becker.lb@outlook.com>
2023-06-05 15:44:43 +0200

    web requests return db data now. Added welcome screen in html/index.html

---
+++ src/db_utils.rs
@@ -0,0 +1,37 @@
+use std::env;
+use diesel::{Connection, PgConnection, QueryDsl, RunQueryDsl, SelectableHelper};
+use diesel::associations::HasTable;
+use dotenvy::dotenv;
+use crate::part::part::Part;
+use crate::schema::parts::dsl::parts;
+
+pub fn create_connection() -> PgConnection{
+    dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set!");
+    PgConnection::establish(&db_url).unwrap_or_else(|_| panic!("Connection failed!"))
+
+}
+
+
+pub fn get_parts_from_db() -> Vec<Part> {
+    let connection= &mut create_connection();
+
+    let result = parts
+        .limit(5)
+        .select(Part::as_select())
+        .load(connection)
+        .expect("Error loading parts");
+    return result
+}
+
+
+pub fn add_part_to_db(new_part:Part){
+    let connection = &mut create_connection();
+    diesel::insert_into(parts::table())
+        .values(&new_part)
+        .returning(Part::as_returning())
+        .get_result(connection)
+        .expect("Part could not be added");
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 pub mod part;
 pub mod schema;
+pub mod db_utils;
 
+use std::fs;
 use actix_web::{App, get, HttpResponse, HttpServer, main, post, Responder, web};
 use actix_web::error::ErrorNotFound;
-use diesel::prelude::*;
-use diesel::pg::PgConnection;
-use dotenvy::dotenv;
-use std::env;
-use diesel::associations::HasTable;
-
-use crate::part::part::{get_example_parts, Part};
-use crate::schema::parts::dsl::parts;
-
+use crate::part::part::Part;
 
 #[derive(Debug)]
 struct PartNotFoundErr{
@@ -20,10 +14,11 @@ struct PartNotFoundErr{
 
 #[get("/")]
 async fn hello() -> impl Responder{
-    HttpResponse::Ok().body("Hello World!")
+    let content = fs::read_to_string("html/index.html").expect("File could not be read!");
+    HttpResponse::Ok().body(content)
 }
 
-#[get("/test/{part_id}")]
+#[get("/{part_id}")]
 async fn get_part(path:web::Path<i32>) -> actix_web::Result<String>{
     let part_id = path.into_inner();
 
@@ -45,13 +40,11 @@ async fn echo(req_body:String) -> impl Responder{
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder{
-    HttpResponse::Ok().body("Hey there!")
-}
+
 
 
 fn check_part_exists(part_id:i32) -> Result<Part,PartNotFoundErr> {
-    let parts_fetched = get_example_parts();
+    let parts_fetched = db_utils::get_parts_from_db();
 
     for p in parts_fetched{
         if p.id == part_id{
@@ -63,48 +56,13 @@ fn check_part_exists(part_id:i32) -> Result<Part,PartNotFoundErr> {
 
 }
 
-fn establish_db_connection() -> PgConnection{
-    dotenv().ok();
-    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set!");
-    PgConnection::establish(&db_url).unwrap_or_else(|_| panic!("Connection failed!"))
-}
-
-fn get_parts_from_db(connection: &mut PgConnection) -> Vec<Part> {
-    let result = parts
-        .limit(5)
-        .select(Part::as_select())
-        .load(connection)
-        .expect("Error loading parts");
-    return result
-}
-
-fn add_part_to_db(connection: &mut PgConnection, new_part:Part){
-    diesel::insert_into(parts::table())
-        .values(&new_part)
-        .returning(Part::as_returning())
-        .get_result(connection)
-        .expect("Part could not be added");
-}
-
 #[main]
 async fn main() -> std::io::Result<()>{
-    let mut conn = establish_db_connection();
-    let examples = get_example_parts();
-    for p in examples{
-        add_part_to_db(&mut conn, p);
-    }
-    let res = get_parts_from_db(&mut conn);
-    for p in res{
-        println!("{}",p.id);
-        println!("{}",p.description);
-        println!("----");
-    }
     HttpServer::new(||{
         App::new()
             .service(hello)
             .service(echo)
             .service(get_part)
-            .route("/hey",web::get().to(manual_hello))
     }).bind(("127.0.0.1",7878))?
         .run()
         .await
